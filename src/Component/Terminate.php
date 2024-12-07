addc69c65ca4481c3193c25d70fb8548fc88a37e
devuri <devuri@users.noreply.github.com>
2024-12-07 17:42:02 -0500

    chore: css update

--- src/Component/Terminate.php
+++ src/Component/Terminate.php
@@ -94,9 +94,15 @@ class Terminate
     {
         $this->pageHeader($pageTitle);
         ?>
-            <div id="error-page" class="">
-                <h1>Exception</h1>
-                <p><?php echo htmlspecialchars($this->exception->getMessage(), ENT_QUOTES, 'UTF-8'); ?></p>
+            <div id="error-page" class="" style="margin-top: 4em; padding: 1.4em; background: #fff;">
+                <h1 style="font-style: oblique;font-weight: 400;margin-bottom: 1em;">
+                    Exception
+                </h1>
+                <div style="margin-bottom: 2em;">
+                    <p>
+                        <?php echo htmlspecialchars($this->exception->getMessage(), ENT_QUOTES, 'UTF-8'); ?>
+                    </p>
+                </div>
                 <p>
                     <?php echo $this->linkUrl(); ?>
                 </p>
@@ -116,7 +122,7 @@ class Terminate
         $path = htmlspecialchars(($this->request['path'] ?? null), ENT_QUOTES);
         $linkedUrl = "{$path}";
 
-        return '<a class="button btn" href="' . $linkedUrl . '">Retry</a>';
+        return '<a class="btn btn-outline-dark" href="' . $linkedUrl . '">Retry</a>';
     }
 
     /**
@@ -197,88 +203,30 @@ class Terminate
         ?>
         <!DOCTYPE html><html lang='en'>
         <head>
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
-            <meta http-equiv="Content-Type" content="text/html; charset='UTF-8'" />
-            <meta name="viewport" content="width=device-width">
+			<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+            <link href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.min.css" rel="stylesheet">
+            <meta charset="utf-8">
+			<meta name="robots" content="noindex, nofollow">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+
+			<!-- Fonts -->
+	        <link rel="preconnect" href="https://fonts.bunny.net">
+	        <link href="https://fonts.bunny.net/css?family=figtree:300,400,500,600" rel="stylesheet" />
+
             <title><?php echo $pageTitle; ?></title>
-            <?php self::pageStyles(); ?>
         </head>
-        <body id="page">
+        <body id="page" style="background: #efefef;">
         <?php
     }
 
     private function pageFooter(): void
     {
         ?>
-        <footer align="center">
-            Status Code:<span style="color:#afafaf"><?php echo (string) $this->statusCode; ?></span>
+        <footer align="center" style="margin-top: 0px; padding: 1em; text-align: end; font-size: small;">
+            Status Code: <span style="color:#afafaf"><?php echo (string) $this->statusCode; ?></span>
             </footer>
             </body>
         </html>
-        <?php
-    }
-
-    private static function pageStyles(): void
-    {
-        ?>
-        <style type="text/css">
-            html {
-                background: #f1f1f1;
-            }
-            body {
-                color: #444;
-                margin: 2em auto;
-                font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
-                padding: 0;
-            }
-            h1 {
-                clear: both;
-                color: #0073aa;
-                font-size: 24px;
-                margin: 0 0 0 0;
-                padding: 0;
-                padding-bottom: 7px;
-                font-weight: inherit;
-            }
-            footer {
-                clear: both;
-                color: #cdcdcd;
-                margin-top: 0px !important;
-                margin: 30px 0 0 0;
-                padding-bottom: 24px !important;
-                padding: 24px;
-                padding-bottom: 7px;
-                font-size: small;
-                text-transform: uppercase;
-            }
-            samp {
-                color: unset;
-                background: none;
-                font-size: 1em;
-            }
-            #error-page {
-                background: #fff;
-                margin-top: 50px;
-                -webkit-box-shadow: 0 1px 1px rgba(0, 0, 0, .04);
-                box-shadow: 0 1px 1px rgba(0, 0, 0, .04);
-                padding: 1.6em 2em;
-            }
-            #error-page p,
-            #error-page .die-message {
-                line-height: 1.5;
-                margin: 25px 0 20px;
-            }
-            #error-page code {
-                font-family: Consolas, Monaco, monospace;
-            }
-            ul li {
-                margin-bottom: 10px;
-                font-size: 14px ;
-            }
-            a {
-                color: #0073aa;
-            }
-        </style>
         <?php
     }
 }
