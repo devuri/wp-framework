0a7ecc9422ba402858f994bc3077fc0c65cbf03b
devuri <devuri@users.noreply.github.com>
2025-01-12 20:12:10 -0500

    fix: adds `::init` useful in cases where a fresh instance of the `DB` class is required instead of a singleton

--- src/Component/Support/DBFactory.php
+++ src/Component/Support/DBFactory.php
@@ -15,6 +15,9 @@ class DBFactory
 {
     private static $instance = null;
 
+    // always gets new instance
+    private $db;
+
     /**
      * Creates or retrieves the existing DB instance.
      *
@@ -39,6 +42,27 @@ class DBFactory
         }
 
         return self::$instance;
+    }
+
+    /**
+     * Creates a new instance of the `DB` class for the specified table.
+     *
+     * This is useful in cases where a fresh instance of the `DB` class
+     * is required instead of a singleton.
+     *
+     * @param string $tableNameNoPrefix The name of the database table without the prefix.
+     *
+     * @return DB An initialized `DB` instance with the specified table.
+     */
+    public static function init(string $tableNameNoPrefix): DB
+    {
+        return (new DB(
+            (string) env('DB_HOST'),
+            (string) env('DB_NAME'),
+            (string) env('DB_USER'),
+            (string) env('DB_PASSWORD'),
+            (string) env('DB_PREFIX')
+        ))->table($tableNameNoPrefix);
     }
 
     /**
